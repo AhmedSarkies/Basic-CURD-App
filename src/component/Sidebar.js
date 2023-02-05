b8ee1ef7a31437fd0be34643b7050af71c70afc3
sarkies <ahmed.sarkes@gmail.com>
2023-02-06 01:16:14 +0200

    Second Commit

---
+++ src/component/Sidebar.js
@@ -0,0 +1,16 @@
+function Sidebar() {
+    return (
+        <>
+        <ul className="list-unstyled">
+            <li>
+                <a className="text-decoration-none fw-bold text-info" href="/#">Get All Products</a>
+            </li>
+            <li>
+                <a className="text-decoration-none fw-bold text-info" href="/#">Get All Categories</a>
+            </li>
+        </ul>
+        </>
+    )
+}
+
+export default Sidebar;
