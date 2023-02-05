793ec02eef57e488a5c7d3ecb1200ac8793d1044
sarkies <ahmed.sarkes@gmail.com>
2023-02-06 01:15:55 +0200

    Second Commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,42 @@
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="/#">
+          CURD App
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link text-black fw-bold"
+                aria-current="page"
+                href="/#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-black fw-bold" href="/#">
+                About
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
