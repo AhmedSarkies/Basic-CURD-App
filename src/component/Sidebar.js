66c901edb7e7c1d7e22ead032fd602f15c0d2f3b
sarkies <ahmed.sarkes@gmail.com>
2023-02-06 23:44:29 +0200

    Third Commit

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,16 +1,33 @@
+import { Link } from "react-router-dom";
+
 function Sidebar() {
-    return (
-        <>
-        <ul className="list-unstyled">
-            <li>
-                <a className="text-decoration-none fw-bold text-info" href="/#">Get All Products</a>
-            </li>
-            <li>
-                <a className="text-decoration-none fw-bold text-info" href="/#">Get All Categories</a>
-            </li>
-        </ul>
-        </>
-    )
+  return (
+    <>
+      <ul className="list-unstyled">
+        <li>
+          <Link className="text-decoration-none fw-bold text-info" to="/">
+            Get Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-decoration-none fw-bold text-info"
+            to="products"
+          >
+            Get All Products
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-decoration-none fw-bold text-info"
+            to="categories"
+          >
+            Get All Categories
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default Sidebar;
