cc4cd862353bab5fcf453c8246b36a52916a20a0
Ahmed Sarkies <AhmedSarkies@gmail.com>
2023-02-11 03:21:37 +0200

    Some Updates

--- src/Pages/ViewProduct.js
+++ src/Pages/ViewProduct.js
@@ -1,7 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function ViewProduct() {
-    return(
-        <h1>View Product</h1>
-    )
+  let { productID } = useParams();
+  const url_api = "http://localhost:9000/products";
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    axios.get(`${url_api}/${productID}`)
+      .then((res) => {
+        setProduct(res.data);
+      });
+  }, []);
+  return (
+    <>
+        <div className="text-center">
+          <h1>{product.title}</h1>
+          <h2>{product.category}</h2>
+          <h3>{product.description}</h3>
+        </div>
+    </>
+  );
 }
 
 export default ViewProduct;
